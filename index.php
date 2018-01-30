8623f668f9af154c1c6a4571e05a075ccc3ae36b
Rodo <rodo552009@hotmail.com>
2018-01-29 18:31:30 -0600

    nuevos archivos

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ejercisio de prueba</title>
+  </head>
+  <body>
+    <h1>Ejercisio de prueba...
+    </h1>
+  </body>
+</html>
