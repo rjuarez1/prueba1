2205a4af5a8cacb20a8d8f34565623af9b1b9dec
Rodo <rodo552009@hotmail.com>
2018-01-30 10:17:38 -0600

    agregamos texto

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     <title>Ejercisio de prueba</title>
   </head>
   <body>
-    <h1>Ejercisio de prueba...
-      Ejercisio de prueba... esto es otra prueba
+    <h1>
+      Ejercisio de prueba...
+      esto es otra prueba
     </h1>
   </body>
 </html>
